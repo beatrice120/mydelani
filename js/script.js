e6853e1c1df9263d8f01e97ffc201d36e3190286
beatrice120 <beatrice120>
2020-10-06 08:32:26 +0200

    hover on portfolio

--- js/script.js
+++ js/script.js
@@ -23,5 +23,57 @@ $(document).ready(function(){
    $(".zela").click(function(){
     $(".denu").show();
     $(".zela").hide();
-   });
+   }); 
+$("#ven1").mouseover(function(){
+    $(".wor1").show();
+    });
+    $("#ven1").mouseleave(function(){
+    $(".wor1").hide();
+    });
+    
+ $("#ven2").mouseover(function(){
+        $(".wor2").show();
+        });
+        $("#ven2").mouseleave(function(){
+        $(".wor2").hide();
+        });
+ $("#ven3").mouseover(function(){
+            $(".wor3").show();
+            });
+            $("#ven3").mouseleave(function(){
+            $(".wor3").hide();
+            });
+ $("#ven4").mouseover(function(){
+                $(".wor4").show();
+                });
+                $("#ven4").mouseleave(function(){
+                $(".wor4").hide();
+                });
+ $("#ven5").mouseover(function(){
+                    $(".wor5").show();
+                    });
+                    $("#ven5").mouseleave(function(){
+                    $(".wor5").hide();
+                    });
+    $("#ven6").mouseover(function(){
+                        $(".wor6").show();
+                        });
+                        $("#ven6").mouseleave(function(){
+                        $(".wor6").hide();
+                        });
+    
+     $("#ven7").mouseover(function(){
+                            $(".wor7").show();
+                            });
+                            $("#ven7").mouseleave(function(){
+                            $(".wor7").hide();
+                            });
+
+    $("#ven8").mouseover(function(){
+                                $(".wor8").show();
+                                });
+                                $("#ven8").mouseleave(function(){
+                                $(".wor8").hide();
+                                });
 });
+
